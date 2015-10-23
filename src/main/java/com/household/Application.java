9cece649c8c24015601060509459695bbcc98b7b
Vlasov Artem <vlasovartem21@gmail.com>
2015-10-23 15:20:48 +0300

    Change structure of the packages

---
+++ src/main/java/com/household/Application.java
@@ -0,0 +1,15 @@
+package com.household;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by artemvlasov on 03/09/15.
+ */
+//@SpringBootApplication
+public class Application {
+//    public static void main(String[] args) {
+//        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//    }
+}
